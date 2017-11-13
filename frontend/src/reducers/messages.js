62e8aca21a77802f25f7961361a38cf19f81d49e
rogeralbinoi <roger.albino.i@gmail.com>
2017-11-12 22:47:04 -0200

    fix: New Message based on state

--- frontend/src/reducers/messages.js
+++ frontend/src/reducers/messages.js
@@ -6,7 +6,7 @@ const initialState = []
 const messages = (state = initialState, action = '') => {
   switch (action.type) {
     case NEW_MESSAGE:
-      return action.message
+      return [...state, ...action.message]
     case REMOVE_MESSAGE:
       return state.filter((message) => message.id !== action.message.id)
     case CLEAR_MESSAGES:
